79f575c0fe01a768c285cc2b295a90b9f4e0efa7
Chemin Elisée <elisee.chemin@etu.univ-grenoble-alpes.fr>
2023-11-07 09:47:19 +0100

    Changement du système de timer

--- src/assets/Utils.py
+++ src/assets/Utils.py
@@ -1,10 +1,9 @@
 #Passer un temps en nombre de frame et le niveau de difficulté de 1 à 10
-import math
-def getFramerateForLevel(base_framerate, level):
-    if base_framerate <= 0 or (1 > level or level > 10):
+import math    
+def getMaxTimeForLevel(timeLvlZero, level):
+    if timeLvlZero <= 0 or 1 > level :
         raise ValueError
     
     #nbr de frames
     # return (math.pow(1.5, level) / math.pow(1.5, 10)) * base_framerate
-    return ((level + base_framerate) / (10 + base_framerate)) * 60
-    
+    return (-0.9 * level) + timeLvlZero
--- src/assets/mj_crous.py
+++ src/assets/mj_crous.py
@@ -11,27 +11,24 @@ class mj_crous(AbstractMiniJeu):
         self.m_level = level
 
     def run_miniJeu(self):
-        framerate = Utils.getFramerateForLevel(60, self.m_level)
+        # framerate = Utils.getFramerateForLevel(60, self.m_level)
         clock = pygame.time.Clock()
-        running = True
         timer = 0
-        max_time = 120
-        timer_width = max_time * 1.5
+        max_time = Utils.getMaxTimeForLevel(10,self.m_level)
+        timer_width = 600
         
 
         sprite_group = pygame.sprite.Group()
         mid_x = screen.get_width() / 2
         mid_y = screen.get_height() / 2
 
-        #stars = [Star(mid_x-200, mid_y-100, True), Star(mid_x, mid_y-100), Star(mid_x+200, mid_y-100),
-        #         Star(mid_x-200, mid_y+100), Star(mid_x, mid_y+100), Star(mid_x+200, mid_y+100)]
         stars = []
         for i in range(self.m_level + 3):
             stars.append(newStar(200, 200, screen.get_width()-200,screen.get_height()-200, stars))  
         stars[0] = Star(stars[0].rect.x, stars[0].rect.y, True)
 
         sprite_group.add(stars)
-        while (running and timer < max_time):
+        while (timer < max_time):
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                      for sprite in sprite_group:
@@ -43,15 +40,15 @@ class mj_crous(AbstractMiniJeu):
             #^ screen.blit du background
             sprite_group.draw(screen)
 
-            barre_w = timer_width - (timer * 1.5)
+            barre_w = timer_width * (1-(timer/max_time))
             loading_bar_rect = pygame.Rect(mid_x-(timer_width/2), mid_y-250, barre_w, 20)
             pygame.draw.rect(screen, "red", loading_bar_rect)
             
             
             pygame.display.update()
             
-            timer +=1
-            clock.tick(framerate)
+            timer += clock.tick(30)/1000
+            
         
         return False
     
