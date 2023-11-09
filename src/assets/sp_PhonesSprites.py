51e46c5e6046ba5e078bed0c644e43246167cd42
lyn3nd <chisanayagi@gmail.com>
2023-11-09 21:44:53 +0100

    blanchon sound fixed

--- src/assets/sp_PhonesSprites.py
+++ src/assets/sp_PhonesSprites.py
@@ -13,18 +13,25 @@ class Teacher(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (self.BASE_X, self.BASE_Y)
         self.isWatching = False
+        self.hasBeenPlayed = False
 
     def update(self):
+
         if(self.duree < 25):
             #quand il se retourne il bouge
             self.isWatching = True
             self.rect.center = (Teacher.WATCHING_POS)
             self.duree+=1
+            if self.hasBeenPlayed == False: #si son n'a pas été joué
+                Utils.play_sound_effect(pygame.mixer.Sound(os.path.dirname(__file__) + "/sounds/mj_phone_spotted.mp3")) #on joue
+                self.hasBeenPlayed = True #et il est considéré comme joué
         else :
             #il revient a la normale
             self.rect.center = (self.BASE_X, self.BASE_Y)
             self.isWatching = False
             self.duree = 0
+            self.hasBeenPlayed = False #le son peut être joué à nouveau
+    
         
     
 class Student(pygame.sprite.Sprite):
